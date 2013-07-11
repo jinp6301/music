f2031f83fb9c607707836a56442f3b20bad1c8c1
Samir and Jin <samir+jin@appacademy.io>
2013-07-11 09:04:06 -0400

    Do migration for band model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Music::Application.config.secret_token = '32ce3e85873410c700f0b087dfb170e22476cab34d460df751a9981a20000a92d6b13aec33a60f4c63d25c27ca07e33f02a840b6b59e218cc8fadcbb84af1c33'

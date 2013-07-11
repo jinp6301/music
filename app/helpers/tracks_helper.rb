68ab06dbe8d08e6803474bf6ea7b50c797ae0d0a
Samir and Jin <samir+jin@appacademy.io>
2013-07-11 17:06:46 -0400

    Add #ugly_lyrics helper to uglify lyrics

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,9 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    ("<pre>" +
+    lyrics.split("\n").map do |line|
+      "&#9835; #{line}"
+    end.join("\n") +
+    "</pre>").html_safe
+  end
 end

263d580dcc8d35bdd95b43fb1c397a2644acb9fb
Samir and Jin <samir+jin@appacademy.io>
2013-07-11 15:04:40 -0400

    Do migration to create users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130711184644) do
+ActiveRecord::Schema.define(:version => 20130711190416) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(:version => 20130711184644) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "lyrics"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "token"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end

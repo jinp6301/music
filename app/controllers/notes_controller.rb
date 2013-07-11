d9c2c09035525f5d60b94bdfd138ecfe83b60f98
Samir and Jin <samir+jin@appacademy.io>
2013-07-11 16:30:12 -0400

    Add user ID to notes when they're created

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   def create
+    params[:note][:user_id] = User.find_by_token(session[:token]).id
     Note.create(params[:note])
     redirect_to track_url(params[:note][:track_id])
   end

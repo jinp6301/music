351fa11a2f570a78cf69815dbfcff1b09b8fdf9d
Samir and Jin <samir+jin@appacademy.io>
2013-07-11 11:37:05 -0400

    Add validation to Band and Album models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,9 @@
 class Album < ActiveRecord::Base
-  attr_accessible :band_id, :name
+  attr_accessible :band_id, :name, :type
+
+  validates :band_id, :name, :type, presence: true
+  validates :name, uniqueness: {scope: :band_id}
+  validates :type, inclusion: { in: ["live", "studio"] }
 
   belongs_to :band, dependent: :destroy
 
--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,9 @@
 class Band < ActiveRecord::Base
   attr_accessible :name
 
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
   has_many :albums
 
 end

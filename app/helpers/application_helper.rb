782f6d34544c35af361106bc6fa327ba1c290b3e
Samir and Jin <samir+jin@appacademy.io>
2013-07-11 11:13:45 -0400

    Move edit link from partial to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def edit_link(name)
+    link_to "Edit #{name}", send("edit_#{name}_url")
+  end
+
   def destroy_button(object)
     button_to "Delete #{object.class.name.downcase}", object, method: :delete
   end

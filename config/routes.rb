3c1d2e7e926394b86a2918e142c7853de6c74ac6
Samir and Jin <samir+jin@appacademy.io>
2013-07-11 09:11:24 -0400

    Add routes for band, album and track models

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,8 @@ Music::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  resources :bands
+  resources :albums
+  resources :tracks
 end
